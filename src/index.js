2a264059186b10d477d2aabe1ce7197ea353cb47
antonella <divirgilio.antonella@gmail.com>
2019-10-30 01:38:05 -0300

    add loader, load random image, set image author

--- src/index.js
+++ src/index.js
@@ -4,47 +4,77 @@ import ReactDOM from "react-dom";
 // Styles
 import "./styles.css";
 
+// Material UI
+import { makeStyles } from '@material-ui/core/styles';
+import { Card, CardMedia, CardContent, CardActions, Button, Typography, Hidden, CircularProgress } from '@material-ui/core';
+
+
 function App() {
-  const [imageUrlId, setImageUrlId] = useState(1);
-  const [imageUrl, setImageUrl] = useState(
-    `https://picsum.photos/id/${imageUrlId}/1024/768`
-  );
+  const [loading, setLoading] = useState(true);
+  const [imageId, setImageId] = useState(1);
+  const [imageAuthor, setImageAuthor] = useState('');
 
   useEffect(() => {
     const timer = setInterval(() => {
-      setImageUrlId(imageUrlId => imageUrlId + 1);
+      setImageId(Math.floor(Math.random() * (1000 + 1)));
     }, 5000);
     return () => clearInterval(timer);
   }, []);
 
   useEffect(() => {
-    setImageUrl(`https://picsum.photos/id/${imageUrlId}/1024/768`);
-  }, [imageUrlId]);
+    const imageInfoUrl = `https://picsum.photos/id/${imageId}/info`;
 
-  useEffect(() => {
-    console.log("imageUrl", imageUrl);
+    const authorPromise = fetch(imageInfoUrl).then(response => response.json());
 
-    fetch(imageUrl)
-      .then(response => {
-        return setImageUrl(response.url);
-      })
-      .catch(error => {
-        console.log("Error getting image: ", error);
-      });
-  }, [imageUrl]);
+    authorPromise.then((response) => {
+      setImageAuthor(response.author);
+      setLoading(false);
+    });
+  }, [imageId]);
+
+  const useStyles = makeStyles({
+    card: {
+      maxWidth: 345,
+    },
+  });
+
+  const classes = useStyles();
 
   return (
     <div className="App">
       <h1>Lorem Picsum</h1>
-      <div className="image-container" />
+      {
+        loading ? <CircularProgress /> :
+          <Card className={classes.card}>
+            <CardMedia
+              component="img"
+              height="240"
+              image={`https://picsum.photos/id/${imageId}/1024/768`}
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="h2">
+                {imageAuthor}
+              </Typography>
+            </CardContent>
+          </Card>
+      }
+
       <style>{`
         .image-container {
-          background-image: url(${imageUrl});
-          min-width: 150px;
+          background-image: url('https://picsum.photos/id/${imageId}/1024/768');         
           width: 100%;
-          min-height: 200px;
+          min-height: 400px;
           background-size: cover;
           background-position: 50%;
+          display: flex;
+          align-items: flex-end;
+        }
+
+        .image-author {
+          margin: 0;
+          padding: 10px;
+          background-color: #000;
+          color: #fff;
         }
       `}</style>
     </div>
